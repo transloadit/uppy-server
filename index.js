939d372ac97dc42b50d4827b474d0f8aa2598720
Harry Hedger <hedgerh@gmail.com>
2015-12-17 10:57:35 -0500

    Adding Koa server

---
+++ index.js
@@ -0,0 +1,22 @@
+var koa = require('koa');  
+var router = require('koa-router')();
+var session = require('koa-session');
+var mount = require('koa-mount');
+var Grent = require('grant-koa');
+var grant = new Grant({});
+
+router.get('/', function *(next) {
+  this.body = 'Hello World';
+});
+
+var app = koa();
+
+app.keys = ['grant'];
+app.use(session(app));
+
+app.use(router.routes())
+app.use(router.allowedMethods());
+
+app.listen(3000);
+
+console.log('Listening on port 3000');

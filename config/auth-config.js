e0b48fd2e672d748f0a83ae410208d28b748b66f
Harry Hedger <hedgerh@gmail.com>
2015-12-21 10:43:32 -0500

    Dropbox oauth flow and routes

---
+++ config/auth-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  dropbox: {
+    clientKey: '982dibzeipfg5fj',
+    clientSecret: 'tvgqmt4emr9ukuj'
+  }
+}
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var koa = require('koa');  
+var koa = require('koa'); 
 var router = require('koa-router')();
 var session = require('koa-session');
 var mount = require('koa-mount');
 var Grant = require('grant-koa');
+var authConfig = require('./config/auth-config');
 var grant = new Grant({
   "server": {
     "protocol": "http",
@@ -12,24 +13,35 @@ var grant = new Grant({
     "state": true
   },
   "dropbox": {
-    "key": "",
-    "secret": "",
+    "key": authConfig.dropbox.clientKey,
+    "secret": authConfig.dropbox.clientSecret,
     "scope": [],
     "callback": "/dropbox/callback"
   }
 });
 
-router.get('/', function *(next) {
-  this.body = 'Hello World';
-});
-
 var app = koa();
+require('koa-qs')(app);
 
 app.keys = ['grant'];
 app.use(session(app));
+app.use(mount(grant))
 
 app.use(router.routes())
 app.use(router.allowedMethods());
+
+router.get('/dropbox/callback', function *(next) {
+  this.body = JSON.stringify(this.query, null, 2);
+});
+
+router.get('/dropbox/fetch', function *(next) {
+  this.redirect('http://www.dropbox.com/1/oauth2/authorize?response_type=code&client_id=' + authConfig.dropbox.clientKey + '&redirect_uri=http://localhost:3000/dropbox/callback');
+})
+
+router.get('/', function *(next) {
+  // console.log(this.session);
+})
+
 
 app.listen(3000);
 

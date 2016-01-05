cf1bbc0eb3aa6ad590cc51177dc7669a4dd88546
Harry Hedger <hedgerh@gmail.com>
2016-01-05 12:20:01 -0500

    Add server folder, controllers, routes.js

--- config/auth-config.js
+++ config/auth-config.js
@@ -2,5 +2,13 @@ module.exports = {
   dropbox: {
     clientKey: '982dibzeipfg5fj',
     clientSecret: 'tvgqmt4emr9ukuj'
+  },
+  google: {
+    clientKey: '982dibzeipfg5fj',
+    clientSecret: 'tvgqmt4emr9ukuj'
+  },
+  instagram: {
+    clientKey: '982dibzeipfg5fj',
+    clientSecret: 'tvgqmt4emr9ukuj'
   }
 }
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var koa = require('koa')
 var request = require('request')
 var router = require('koa-router')()
+var routes = require('./server/routes');
 var session = require('koa-session')
 var mount = require('koa-mount')
 var Grant = require('grant-koa')
@@ -43,20 +44,21 @@ app.use(mount(grant))
 app.use(router.routes())
 app.use(router.allowedMethods())
 
-function authCallbackHandler(provider, code) {
+function authCallbackHandler(provider) {
   if (!this.session.tokens) {
     this.session.tokens = {}
   }
-
+  console.log(this.session.tokens)
+  console.log(this.query.code)
   request.post({
     url: auth[provider].tokenURI,
     json: true,
     qs: {
-      code: code,
+      code: this.query.code,
       grant_type: 'authorization_code',
       client_id: auth[provider].clientKey,
       client_secret: auth[provider].clientSecret,
-      redirect_uri: auth[provider].redirectURI
+      redirect_uri: 'http://localhost:3000/dropbox/callback'
     },
     headers: [
       {
@@ -75,27 +77,31 @@ function authCallbackHandler(provider, code) {
 }
 
 router.get('/dropbox/callback', function *(next) {
-  authCallbackHandler('dropbox', this.query.code)
-}
+  authCallbackHandler.bind(this)('dropbox');
+})
 
 router.get('/google/callback', function *(next) {
-  authCallbackHandler('google', this.query.code)
-}
+  authCallbackHandler('google').bind(this)
+})
 
 router.get('/instagram/callback', function *(next) {
-  authCallbackHandler('instagram', this.query.code)
-}
+  authCallbackHandler('instagram').bind(this)
+})
 
 router.get('/dropbox/connect', function *(next) {
-  this.redirect('http://www.dropbox.com/1/oauth2/authorize?response_type=code&client_id=' + authConfig.dropbox.clientKey + '&redirect_uri=http://localhost:3000/dropbox/callback')
+  this.redirect('http://www.dropbox.com/1/oauth2/authorize?response_type=code&client_id=' + auth.dropbox.clientKey + '&redirect_uri=http://localhost:3000/dropbox/callback')
+})
+
+router.get('/dropbox/connect', function *(next) {
+  this.redirect('http://www.dropbox.com/1/oauth2/authorize?response_type=code&client_id=' + auth.dropbox.clientKey + '&redirect_uri=http://localhost:3000/dropbox/callback')
 })
 
 router.get('/google/connect', function *(next) {
-  this.redirect('http://www.google.com/1/oauth2/authorize?response_type=code&client_id=' + authConfig.google.clientKey + '&redirect_uri=http://localhost:3000/google/callback')
+  this.redirect('http://www.google.com/1/oauth2/authorize?response_type=code&client_id=' + auth.google.clientKey + '&redirect_uri=http://localhost:3000/google/callback')
 })
 
 router.get('/instagram/connect', function *(next) {
-  this.redirect('http://www.instagram.com/1/oauth2/authorize?response_type=code&client_id=' + authConfig.instagram.clientKey + '&redirect_uri=http://localhost:3000/instagram/callback')
+  this.redirect('http://www.instagram.com/1/oauth2/authorize?response_type=code&client_id=' + auth.instagram.clientKey + '&redirect_uri=http://localhost:3000/instagram/callback')
 })
 
 router.get('/', function *(next) {

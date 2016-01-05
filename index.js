9dd506951f018cf2a607cf4ade4fe5eab05dd217
Harry Hedger <hedgerh@gmail.com>
2016-01-05 09:46:26 -0500

    Added instagram and google. Refactored controller

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var koa = require('koa'); 
-var request = require('request');
-var router = require('koa-router')();
-var session = require('koa-session');
-var mount = require('koa-mount');
-var Grant = require('grant-koa');
-var authConfig = require('./config/auth-config');
+var koa = require('koa')
+var request = require('request')
+var router = require('koa-router')()
+var session = require('koa-session')
+var mount = require('koa-mount')
+var Grant = require('grant-koa')
+var auth = require('./config/auth-config')
 var grant = new Grant({
   "server": {
     "protocol": "http",
@@ -14,38 +14,49 @@ var grant = new Grant({
     "state": true
   },
   "dropbox": {
-    "key": authConfig.dropbox.clientKey,
-    "secret": authConfig.dropbox.clientSecret,
+    "key": auth.dropbox.clientKey,
+    "secret": auth.dropbox.clientSecret,
     "scope": [],
     "callback": "/dropbox/callback"
+  },
+  "google": {
+    "key": auth.google.clientKey,
+    "secret": auth.google.clientSecret,
+    "scope": [],
+    "callback": "/google/callback"
+  },
+  "instagram": {
+    "key": auth.instagram.clientKey,
+    "secret": auth.instagram.clientSecret,
+    "scope": [],
+    "callback": "/instagram/callback"
   }
-});
+})
 
-var app = koa();
-require('koa-qs')(app);
+var app = koa()
+require('koa-qs')(app)
 
-app.keys = ['grant'];
-app.use(session(app));
+app.keys = ['grant']
+app.use(session(app))
 app.use(mount(grant))
 
 app.use(router.routes())
-app.use(router.allowedMethods());
+app.use(router.allowedMethods())
 
-router.get('/dropbox/callback', function *(next) {
+function authCallbackHandler(provider, code) {
   if (!this.session.tokens) {
-    this.session.tokens = {};
+    this.session.tokens = {}
   }
-  var code = this.query.code;
 
   request.post({
-    url: 'https://api.dropboxapi.com/1/oauth2/token',
+    url: auth[provider].tokenURI,
     json: true,
     qs: {
       code: code,
       grant_type: 'authorization_code',
-      client_id: authConfig.dropbox.clientKey,
-      client_secret: authConfig.dropbox.clientSecret,
-      redirect_uri: 'http://localhost:3000/dropbox/callback'
+      client_id: auth[provider].clientKey,
+      client_secret: auth[provider].clientSecret,
+      redirect_uri: auth[provider].redirectURI
     },
     headers: [
       {
@@ -55,24 +66,44 @@ router.get('/dropbox/callback', function *(next) {
     ]
   }, (err, res, body) => {
     if (err) {
-      return console.log(err);
+      return console.log(err)
     }
+  
+    this.session.tokens[provider] = body.access_token
 
-    this.session.tokens.dropbox = body.access_token;
+  })
+}
 
-  });
-});
+router.get('/dropbox/callback', function *(next) {
+  authCallbackHandler('dropbox', this.query.code)
+}
 
-router.get('/dropbox/fetch', function *(next) {
-  this.session = null;
-  this.redirect('http://www.dropbox.com/1/oauth2/authorize?response_type=code&client_id=' + authConfig.dropbox.clientKey + '&redirect_uri=http://localhost:3000/dropbox/callback');
+router.get('/google/callback', function *(next) {
+  authCallbackHandler('google', this.query.code)
+}
+
+router.get('/instagram/callback', function *(next) {
+  authCallbackHandler('instagram', this.query.code)
+}
+
+router.get('/dropbox/connect', function *(next) {
+  this.redirect('http://www.dropbox.com/1/oauth2/authorize?response_type=code&client_id=' + authConfig.dropbox.clientKey + '&redirect_uri=http://localhost:3000/dropbox/callback')
+})
+
+router.get('/google/connect', function *(next) {
+  this.redirect('http://www.google.com/1/oauth2/authorize?response_type=code&client_id=' + authConfig.google.clientKey + '&redirect_uri=http://localhost:3000/google/callback')
+})
+
+router.get('/instagram/connect', function *(next) {
+  this.redirect('http://www.instagram.com/1/oauth2/authorize?response_type=code&client_id=' + authConfig.instagram.clientKey + '&redirect_uri=http://localhost:3000/instagram/callback')
 })
 
 router.get('/', function *(next) {
-
+  console.log('index')
 })
 
 
-app.listen(3000);
 
-console.log('Listening on port 3000');
+app.listen(3000)
+
+console.log('Listening on port 3000')

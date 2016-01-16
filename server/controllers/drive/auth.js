e35eda0a61c359e721743cecef5634ffed88dce2
Harry Hedger <hedgerh@gmail.com>
2016-01-15 22:38:52 -0500

    Create new auth/client per request.  Worked out auth issues.

--- server/controllers/drive/auth.js
+++ server/controllers/drive/auth.js
@@ -1,43 +1,51 @@
 var googleAuth = require('google-auth-library')
 var config = require('../../../config/auth')
-var callback = config.server.protocol + '://' + config.server.host  + config.drive.callback
 
-var SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly'];
+var clientKey = config.drive.key
+var clientSecret = config.drive.secret
+var redirectUrl = config.server.url + config.drive.callback
+
+var SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
 
 module.exports = {
-  authorize: function* (next) {
-    if (!this.session.drive) {
-      this.session.drive = {}
-    }
-
-    if (!this.session.drive.auth || !this.session.drive.client) {
+  authorize() {
+    return function* (next) {
+      if (!this.session.drive) {
+        this.session.drive = {}
+      }
       var auth = new googleAuth()
-      var client = new auth.OAuth2(config.drive.key, config.drive.secret, callback)
-      this.session.drive.auth = auth
-      this.session.drive.client = client
-    }
+      var client = new auth.OAuth2(clientKey, clientSecret, redirectUrl)
 
-    if (!this.session.drive.token) {
-      console.log(this.session.drive);
-      var authUrl = this.session.drive.client.generateAuthUrl({
-        access_type: 'offline',
-        scope: SCOPES
-      })
+      if (!this.session.drive.token) {
+        var authUrl = client.generateAuthUrl({
+          access_type: 'offline',
+          scope: SCOPES
+        })
 
-      this.redirect(authUrl)
-    } else {
-      this.session.drive.client.credentials = this.session.drive.token
+        this.redirect(authUrl)
+      } else {
+        this.session.drive.client.credentials = this.session.drive.token
+      }
     }
   },
-  getToken: function* (next) {
-    this.session.drive.client.getToken(this.query.code, function(err, token) {
-      if (err) {
-        console.log('Error while trying to retrieve access token', err)
-        return
+  getToken() {
+    return function* (next) {
+      var auth = new googleAuth()
+      var client = new auth.OAuth2(config.drive.key, config.drive.secret, callback)
+
+      yield function fetchToken(cb) {
+        client.getToken(this.query.code, function tokenCallback(err, token) {
+          if (err) {
+            console.log('Error while trying to retrieve access token', err)
+            return
+          }
+
+          this.session.drive.token = token
+          cb()
+        }.bind(this))
       }
-    this.session.drive.client.credentials = token
-    this.session.drive.token = token
-    this.redirect('/')
-    })
+
+      this.redirect('/')
+    }
   }
 }

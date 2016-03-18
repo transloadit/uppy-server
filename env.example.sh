dfc2877050d3d6809af3dc8b42b269331fc25893
Kevin van Zonneveld <kevin@vanzonneveld.net>
2016-03-18 15:15:12 +0100

    Add master example

--- env.example.sh
+++ env.example.sh
@@ -7,19 +7,12 @@ __file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
 __base="$(basename ${__file} .sh)"
 __rootdir="${__dir}"
 
+export NODE_ENV="${NODE_ENV:-development}"
 export DEPLOY_ENV="${DEPLOY_ENV:-production}"
 export DEBUG="frey:*"
 
-# Secret keys here:
-# export FREY_AWS_ACCESS_KEY="xyz"
-# export FREY_AWS_SECRET_KEY="xyz123"
-# export FREY_AWS_ZONE_ID="Z123"
-# ^-- Travis won't need these. We'll only use Travis for deploys
-
-# export FREY_DOMAIN="server.uppy.io"
-
-# export FREY_ENCRYPTION_SECRET="abcdefghijklmnopqrstuvwXYZ!"
-
+export FREY_DOMAIN="server.uppy.io"
+# export FREY_ENCRYPTION_SECRET="***"
 export UPPYSERVER_DROPBOX_KEY="***"
 export UPPYSERVER_DROPBOX_SECRET="***"
 export UPPYSERVER_GOOGLE_KEY="***"
@@ -28,6 +21,7 @@ export UPPYSERVER_INSTAGRAM_KEY="***"
 export UPPYSERVER_INSTAGRAM_SECRET="***"
 
 # source env.sh
+# travis encrypt --add env.global "FREY_DOMAIN=${FREY_DOMAIN}"
 # travis encrypt --add env.global "FREY_ENCRYPTION_SECRET=${FREY_ENCRYPTION_SECRET}"
 # travis encrypt --add env.global "UPPYSERVER_DROPBOX_KEY=${UPPYSERVER_DROPBOX_KEY}"
 # travis encrypt --add env.global "UPPYSERVER_DROPBOX_SECRET=${UPPYSERVER_DROPBOX_SECRET}"
---
+++ master.env.example.sh
@@ -0,0 +1,6 @@
+# Secret keys here:
+# export FREY_AWS_ACCESS_KEY="xyz"
+# export FREY_AWS_SECRET_KEY="xyz123"
+# export FREY_AWS_ZONE_ID="Z123"
+
+source env.sh

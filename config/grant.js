42744222d3a8c1f65ddccef7d12dfe53a0d139a6
Kevin van Zonneveld <kevin@vanzonneveld.net>
2016-04-08 22:16:44 +0200

    Fix typo

--- config/grant.js
+++ config/grant.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   google: {
     key: process.env.UPPYSERVER_GOOGLE_KEY,
-    secret: process.env.UPPYSERVER_GOOGLE_SECRETT,
+    secret: process.env.UPPYSERVER_GOOGLE_SECRET,
     scope: [
       'https://www.googleapis.com/auth/drive',
       'https://www.googleapis.com/auth/drive.file'

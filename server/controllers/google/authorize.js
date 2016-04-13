99e78b78e6051f83b91ae2ba1619cfd849987008
Harry Hedger <hedgerh@gmail.com>
2016-04-13 02:26:47 -0400

    Authorize controller checks for expired token during auth check. Cannot issue refresh token yet but returns false authentication.

--- server/controllers/google/authorize.js
+++ server/controllers/google/authorize.js
@@ -1,5 +1,52 @@
 module.exports = function * (next) {
-  this.body = {
-    isAuthenticated: this.session.google.token !== undefined
+  var self = this
+  var Purest = require('purest')
+  var google = new Purest({
+    provider: 'google',
+    api     : 'drive'
+  })
+
+  if (this.session.google.token !== undefined) {
+    yield function checkAuth (cb) {
+      google.get('files', {
+        auth: {
+          bearer: self.session.google.token
+        }
+      }, function (err, res, body) {
+        if (err) {
+          if (err.error.code === 401) {
+            self.session.google.token = null
+            self.body = {
+              isAuthenticated: false
+            }
+            cb()
+            // google.get('', {
+            //   qs: {
+            //     refresh_token: self.session.google.refreshToken
+            //   }
+            // }, function (err, res, body) {
+            //   console.log('we re good')
+            //   self.body = {
+            //     isAuthenticated: true
+            //   }
+            //   console.log(err)
+            //   console.log(res)
+            //   console.log(body)
+            //   cb()
+            // })
+          }
+        } else {
+          console.log('good')
+          self.body = {
+            isAuthenticated: true
+          }
+          cb()
+        }
+      })
+    }
+  } else {
+    this.body = {
+      isAuthenticated: false
+    }
   }
 }

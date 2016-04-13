76d58646f82f25e0748227509c68219d11f62dc6
Harry Hedger <hedgerh@gmail.com>
2016-04-13 02:25:53 -0400

    Removed old GoogleDrive controllers. /google/ is most up to date.

--- server/controllers/drive/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  auth: require('./auth'),
-  get: require('./get'),
-  list: require('./list'),
-  logout: require('./logout')
-}
--- server/controllers/drive/logout.js
+++
@@ -1,7 +0,0 @@
-module.exports = function () {
-  return function * (next) {
-    this.session = null
-    this.body = 'Successfully logged out.'
-    console.log('[logged out]')
-  }
-}

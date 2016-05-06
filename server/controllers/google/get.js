724cb6cb9f24fc5b46a4442ec3beba5e2d97f83f
Kevin van Zonneveld <kevin@vanzonneveld.net>
2016-05-06 21:40:13 +0200

    Add logging for downloading files

--- server/controllers/google/get.js
+++ server/controllers/google/get.js
@@ -18,15 +18,19 @@ module.exports = function * (next) {
   })
 
   yield function getFile (cb) {
+    var url = `files/${this.query.fileId}`
     // First fetch file meta data, not actual file
-    google.get(`files/${this.query.fileId}`, {
+    google.get(url, {
       auth: {
         bearer: this.session.google.token
       }
     }, function (err, res, file) {
       if (err) {
-        throw err
+        throw new Error(`Could not retrieve '${url}' from Google Drive. ${err}`)
       }
+
+      var filepath = `./output/${self.query.fileId}`
+
       // If file is Google document, need to download exported Office doc
       if (file.mimeType.indexOf('application/vnd.google-apps.') !== -1) {
         var exportMimeType = fileTypes[file.mimeType.replace('application/vnd.google-apps.', '')]
@@ -46,7 +50,9 @@ module.exports = function * (next) {
             return cb()
           }
 
-          fs.writeFile(`./output/${self.query.fileId}`, body, {encoding: 'binary'}, function (err, res) {
+          console.log(`Saving exported file with content-type: '${res.headers['content-type']}' as exportMime '${exportMimeType}' to '${filepath}'`)
+
+          fs.writeFile(filepath, body, 'utf8', function (err, res) {
             if (err) {
               console.log(err)
               self.body = err
@@ -71,7 +77,10 @@ module.exports = function * (next) {
             self.body = 'Error: ' + err
             return cb()
           }
-          fs.writeFile(`./output/${file.title}`, body, {encoding: 'binary'}, function (err, res) {
+
+          console.log(`Saving regular file with content-type: '${res.headers['content-type']}' to '${filepath}'`)
+
+          fs.writeFile(filepath, body, 'utf8', function (err, res) {
             if (err) {
               console.log(err)
               self.body = err

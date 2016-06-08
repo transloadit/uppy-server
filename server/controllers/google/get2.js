ba72edacbb610d5ab41ebb7575e0861a4c813ead
Harry Hedger <hedgerh@gmail.com>
2016-06-08 15:03:59 -0400

    Rewrote get controller. Have not integrated yet due to piping error.

--- server/controllers/google/get2.js
+++ server/controllers/google/get2.js
@@ -20,13 +20,69 @@ function isGoogleFile (file) {
   return file.mimeType.indexOf('application/vnd.google-apps.') !== -1
 }
 
-// function getFileType (type) {
-//   return googleFileTypes[type.replace('application/vnd.google-apps.', '')]
-// }
+function getFileExtension (type) {
+  var fileType = googleFileTypes[type.replace('application/vnd.google-apps.', '')]
+  if (!fileType) return
 
-// function getFileMimeType (type) {
-//   return googleFileTypes[type.replace('application/vnd.google-apps.', '')].mimeType
-// }
+  return fileType.extension
+}
+
+function getFileMimeType (type) {
+  var fileType = googleFileTypes[type.replace('application/vnd.google-apps.', '')]
+  if (!fileType) return
+
+  return fileType.mimeType
+}
+
+function getUploadStream (opts, cb, self) {
+  var writer = fs.createWriteStream(opts.fileName)
+
+  writer.on('finish', function () {
+    if (opts.target) {
+      fs.readFile(opts.fileName, function (err, data) {
+        if (err) {
+          console.log(err)
+          return
+        }
+
+        var req = http.request({
+          host: opts.target,
+          method: 'POST',
+          'Content-Type': 'multipart/form-data',
+          'Content-Length': data.length
+        }, (res) => {
+          console.log(`STATUS: ${res.statusCode}`)
+          console.log(`HEADERS: ${JSON.stringify(res.headers)}`)
+          res.on('data', (chunk) => {
+            console.log(`BODY: ${chunk}`)
+          })
+
+          res.on('end', () => {
+            if (res.statusCode >= 200 && res.statusCode <= 300) {
+              self.status = res.status
+              self.statusCode = res.statusCode
+              return cb()
+            }
+            console.log('No more data in response.')
+          })
+        })
+
+        req.on('error', (e) => {
+          console.log(`problem with request: ${e.message}`)
+        })
+
+        req.write(data)
+        req.end()
+      })
+    } else {
+      self.status = 200
+      self.statusText = 'File written to uppy server local storage'
+      return cb()
+    }
+  })
+
+  return writer
+}
 
 /**
  * Fetch a file from Google Drive
@@ -45,143 +101,83 @@ module.exports = function * (next) {
     }
   })
 
-  google.query()
-    .get(`files/${self.request.body.fileId}`)
-    .request((err, res, file) => {
-      if (err) {
-        this.status = 500
-        this.statusText = 'There was an error fetching the file information.'
-        return
-      }
-
-      if (isGoogleFile(file)) {
-
-      }
-    })
+  var fileId = this.request.body.fileId
+  var target = this.request.body.target
 
   yield function getFile (cb) {
-    var writer
-    var url = `files/${self.request.body.fileId}`
-    // First fetch file meta data, not actual file
-    google.get(url, {
-      auth: {
-        bearer: this.session.google.token
-      }
-    }, function (err, res, file) {
-      if (err) {
-        console.log(`Error: Could not retrieve '${url}' from Google Drive. ${err}`)
-        this.status = 500
-        this.statusText = err
-        return cb()
-      }
+    if (!fileId) {
+      console.log('invalid file id: ' + fileId)
+      self.status = 400
+      self.statusText = 'An invalid fileId was provided'
+      return cb()
+    }
 
-      // If file is Google document, need to download exported Office doc
-      if (file.mimeType.indexOf('application/vnd.google-apps.') !== -1) {
-        var fileType = googleFileTypes[file.mimeType.replace('application/vnd.google-apps.', '')]
-        if (!fileType) {
+    google.query()
+      .get('files/' + fileId)
+      .request((err, res, file) => {
+        var writer
+        var opts
+
+        if (err) {
           self.status = 500
-          self.statusText = 'File type not recognized.'
+          self.statusText = 'There was an error fetching the file information.'
           return cb()
         }
 
-        writer = fs.createWriteStream('./output/' + file.title + fileType[1] || 'cat.png')
-        writer.on('finish', function () {
-          fs.readFile('./output/' + file.title + fileType[1], function (err, data) {
-            if (err) { console.log(err) }
+        if (isGoogleFile(file)) {
+          var mimeType = getFileMimeType(file.mimeType)
+          var extension = getFileExtension(file.mimeType)
 
-            var req = http.request({
-              host: self.request.body.target,
-              method: 'POST',
-              'Content-Type': 'multipart/form-data',
-              'Content-Length': data.length
-            }, (res) => {
-              console.log(`STATUS: ${res.statusCode}`)
-              console.log(`HEADERS: ${JSON.stringify(res.headers)}`)
-              res.on('data', (chunk) => {
-                console.log(`BODY: ${chunk}`)
-              })
+          opts = {
+            fileName: './output/' + file.title + extension,
+            target: target
+          }
 
-              res.on('end', () => {
-                if (res.statusCode >= 200 && res.statusCode <= 300) {
-                  self.statusCode = res.statusCode
-                  return cb()
-                }
-                console.log('No more data in response.')
-              })
-            })
+          if (!mimeType) {
+            self.status = 500
+            self.statusText = 'Uppy Server cannot export this type of file'
+            return cb()
+          }
 
-            req.on('error', (e) => {
-              console.log(`problem with request: ${e.message}`)
-            })
+          writer = getUploadStream(opts, cb, self)
 
-            req.write(data)
-            req.end()
+          google.get('files/' + fileId + '/export', {
+            qs: {
+              mimeType: mimeType
+            }
+          }, (err, res, body) => {
+            if (err) {
+              self.status = res.status
+              self.statusText = res.statusText
+              return cb()
+            }
+
+            console.log('Saving exported file with content-type: `' + res.headers['content-type'] + '` as export mimeType `' + mimeType + '` to `./output/' + file.title + extension + '`')
           })
-          // fs.createReadStream('./output/' + file.title + fileType[1] || 'cat.png')
-          // .pipe(http.request({
-          //   host: self.request.body.target,
-          //   method: 'POST',
-          //   'Content-Type': 'application/x-www-form-urlencoded',
-          //   'Content-Length': ''
-          // }, (res) => {
-          //   console.log(res)
-          // }))
-        })
-        // Pass mimeType of desired file type to export
-        google.get(`${url}/export`, {
-          auth: {
-            bearer: self.session.google.token
-          },
-          qs: {
-            mimeType: fileType[0]
-          }
-        }, function (err, res, body) {
-          if (err) {
-            console.log(err)
-            self.body = 'Error: ' + err
-            return cb()
+          .pipe(writer)
+        } else {
+          opts = {
+            fileName: './output/' + file.title,
+            target: target
           }
 
-          console.log(`Saving exported file with content-type: '${res.headers['content-type']}' as exportMime '${fileType[0]}' to './output/${file.title}${fileType[1]}'`)
-          self.body = 'ok'
-          self.status = 200
-          cb()
-        })
-        .pipe(writer)
-      } else {
-        writer = fs.createWriteStream('./output/' + file.title || 'cat.png')
-        writer.on('finish', function () {
-          fs.createReadStream('./output/' + file.title || 'cat.png')
-          .pipe(http.request({
-            host: self.request.body.target,
-            method: 'POST',
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Content-Length': '1000'
-          }, (res) => {
-            console.log(res.statusCode)
-          }))
-        })
-        // Fetch non-Google files
-        google.get(`${url}`, {
-          auth: {
-            bearer: self.session.google.token
-          },
-          qs: {
-            alt: 'media'
-          }
-        }, function (err, res, body) {
-          if (err) {
-            console.log(err)
-            self.body = 'Error: ' + err
-            return cb()
-          }
+          writer = getUploadStream(opts, cb, self)
 
-          console.log(`Saving regular file with content-type: '${res.headers['content-type']}' to './output/${file.title}'`)
-          self.body = 'ok'
-          self.status = 200
-          cb()
-        }).pipe(writer)
-      }
-    })
+          google.get('files/' + fileId, {
+            qs: {
+              alt: 'media'
+            }
+          }, (err, res, body) => {
+            if (err) {
+              self.status = res.status
+              self.statusText = res.statusText
+              return cb()
+            }
+
+            console.log('Saving regular file with content-type: `' + res.headers['content-type'] + '` to `./output/' + file.title + '`')
+          })
+          .pipe(writer)
+        }
+      })
   }
 }

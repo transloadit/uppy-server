8b7e7bce358f8d5f03cd2dea13d8e16a01db6818
Harry Hedger <hedgerh@gmail.com>
2016-07-20 01:36:29 -0400

    Removed websockify. converting to regular websocket connection and using redis/jwt for token generation.

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
 #!/usr/bin/env node
 var koa = require('koa')
-var websockify = require('koa-websocket')
 var session = require('koa-session')
 var cors = require('koa-cors')
 var mount = require('koa-mount')
 var bodyParser = require('koa-bodyparser')
 var Grant = require('grant-koa')
 var grant = new Grant(require('./config/grant'))
-var route = require('koa-route')
+var helpers = require('./utils/helpers')
+
+var SocketServer = require('ws').Server
 
 // websocket utils
-var routeMessage = require('./utils/routeMessage')
-var wrapSend = require('./utils/wrapSend')
+// var routeMessage = require('./utils/routeMessage')
+// var wrapSend = require('./utils/wrapSend')
 
-// google websocket event handlers
-var googleGet = require('./server/websocket/google/get')
-var googleAuth = require('./server/websocket/google/authorize')
-var googleLogout = require('./server/websocket/google/logout')
-var googleList = require('./server/websocket/google/list')
+// // google websocket event handlers
+// var googleGet = require('./server/websocket/google/get')
+// var googleAuth = require('./server/websocket/google/authorize')
+// var googleLogout = require('./server/websocket/google/logout')
+// var googleList = require('./server/websocket/google/list')
 
-var app = websockify(koa())
+var app = koa()
 
 var version = require('./package.json').version
 
 require('koa-qs')(app)
 
 app.keys = ['grant']
-var sess = session(app)
-
-app.use(sess)
-app.ws.use(sess)
+app.use(session(app))
 
 app.use(bodyParser())
 app.use(mount(grant))
@@ -50,28 +48,59 @@ app.use(cors({
 }))
 
 // websocket event subscribers
-app.ws.use(route.all('/', function * (next) {
-  console.log(this.session)
-  this.session = null
-  this.websocket.send = wrapSend(this.websocket.send).bind(this.websocket)
-  app.context.websocket = this.websocket
-  this.websocket.send('uppy.debug', 'websocket init')
-  this.websocket.on('message', routeMessage.bind(this))
-  this.websocket.on('google.get', googleGet.bind(this))
-  this.websocket.on('google.auth', googleAuth.bind(this))
-  this.websocket.on('google.logout', googleLogout.bind(this))
-  this.websocket.on('google.list', googleList.bind(this))
-  this.websocket.on('google.callback', (token) => {
-    if (!this.session.google) {
-      this.session.google = {}
-    }
-    this.session.google.token = token
-    this.websocket.send('google.auth.complete')
-    this.websocket.send('google.auth.pass')
-  })
-}))
+// app.ws.use(route.all('/', function * (next) {
+//   console.log(this.session)
+//   this.session = null
+//   this.websocket.send = wrapSend(this.websocket.send).bind(this.websocket)
+//   app.context.websocket = this.websocket
+//   this.websocket.send('uppy.debug', 'websocket init')
+//   this.websocket.on('message', routeMessage.bind(this))
+//   this.websocket.on('google.get', googleGet.bind(this))
+//   this.websocket.on('google.auth', googleAuth.bind(this))
+//   this.websocket.on('google.logout', googleLogout.bind(this))
+//   this.websocket.on('google.list', googleList.bind(this))
+//   this.websocket.on('google.callback', (token) => {
+//     if (!this.session.google) {
+//       this.session.google = {}
+//     }
+//     this.session.google.token = token
+//     this.websocket.send('google.auth.complete')
+//     this.websocket.send('google.auth.pass')
+//   })
+// }))
+
+function handleAuth (data) {
+  var token = data.token
+
+  if (token === null || typeof token === 'undefined') {
+    console.log('im in here')
+    console.log(data)
+    // token = helpers.generateAndStoreToken(data.upgradeReq)
+    console.log(token)
+    this.websocket.send('uppy.token', {token})
+  }
+  console.log(token)
+  var decoded = helpers.verifyToken(token)
+
+  if (!decoded || !decoded.auth) {
+  }
+
+  this.websocket.sessionId = decoded.auth
+  this.websocket.send('uppy.auth.pass')
+}
 
 require('./server/routes')(app)
+
+var wss = new SocketServer({
+  server: app
+})
+
+wss.on('connection', function (ws) {
+  console.log('Client connected')
+  ws.on('close', function () {
+    console.log('Client disconnected')
+  })
+})
 
 app.listen(3020)
 

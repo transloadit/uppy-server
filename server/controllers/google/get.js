8c08f1b3cc3ab90199c299e70f7570c7b7ee5cf7
Harry Hedger <hedgerh@gmail.com>
2016-07-29 18:32:51 -0400

    Removed tus

--- server/controllers/google/get.js
+++ server/controllers/google/get.js
@@ -1,160 +1,160 @@
-// var fs = require('fs')
-// var http = require('http')
+var fs = require('fs')
+var http = require('http')
 // var path = require('path')
 // var tus = require('tus-js-client')
 // var wss = require('../../../WebsocketServer')
 // var generateUUID = require('../../../utils/generateUUID')
 
-// var googleFileTypes = {
-//   document: {
-//     mimeType: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-//     extension: '.docx'
-//   },
-//   presentation: {
-//     mimeType: 'application/vnd.openxmlformats-officedocument.presentationml.presentation',
-//     extension: '.pptx'
-//   },
-//   spreadsheet: {
-//     mimeType: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-//     extension: '.xlsx'
-//   }
-// }
+var googleFileTypes = {
+  document: {
+    mimeType: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+    extension: '.docx'
+  },
+  presentation: {
+    mimeType: 'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+    extension: '.pptx'
+  },
+  spreadsheet: {
+    mimeType: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    extension: '.xlsx'
+  }
+}
 
-// function isGoogleFile (file) {
-//   return file.mimeType.indexOf('application/vnd.google-apps.') !== -1
-// }
+function isGoogleFile (file) {
+  return file.mimeType.indexOf('application/vnd.google-apps.') !== -1
+}
 
-// function getFileExtension (type) {
-//   var fileType = googleFileTypes[type.replace('application/vnd.google-apps.', '')]
-//   if (!fileType) return
+function getFileExtension (type) {
+  var fileType = googleFileTypes[type.replace('application/vnd.google-apps.', '')]
+  if (!fileType) return
 
-//   return fileType.extension
-// }
+  return fileType.extension
+}
 
-// function getFileMimeType (type) {
-//   var fileType = googleFileTypes[type.replace('application/vnd.google-apps.', '')]
-//   if (!fileType) return
+function getFileMimeType (type) {
+  var fileType = googleFileTypes[type.replace('application/vnd.google-apps.', '')]
+  if (!fileType) return
 
-//   return fileType.mimeType
-// }
+  return fileType.mimeType
+}
 
-// function getUploadStream (opts, cb, self) {
-//   var writer = fs.createWriteStream(opts.fileName)
+function getUploadStream (opts, cb, self) {
+  var writer = fs.createWriteStream(opts.fileName)
 
-//   writer.on('finish', function () {
-//     if (!opts.target) {
-//       self.status = 200
-//       self.statusText = 'File written to uppy server local storage'
-//       return cb()
-//     }
+  writer.on('finish', function () {
+    if (!opts.target) {
+      self.status = 200
+      self.statusText = 'File written to uppy server local storage'
+      return cb()
+    }
 
-//     if (opts.protocol === 'tus') {
-//       var token = generateUUID()
-//       console.log('tus upload')
-//       var filePath = opts.fileName
-//       var file = fs.createReadStream(filePath)
-//       var size = fs.statSync(filePath).size
-//       var options = {
-//         endpoint: opts.target,
-//         resume: true,
-//         metadata: {
-//           filename: path.basename(opts.fileName)
-//         },
-//         uploadSize: size,
-//         onError: function (error) {
-//           throw error
-//         },
-//         onProgress: function (bytesUploaded, bytesTotal) {
-//           var percentage = (bytesUploaded / bytesTotal * 100).toFixed(2)
-//           console.log(bytesUploaded, bytesTotal, percentage + '%')
+    // if (opts.protocol === 'tus') {
+    //   var token = generateUUID()
+    //   console.log('tus upload')
+    //   var filePath = opts.fileName
+    //   var file = fs.createReadStream(filePath)
+    //   var size = fs.statSync(filePath).size
+    //   var options = {
+    //     endpoint: opts.target,
+    //     resume: true,
+    //     metadata: {
+    //       filename: path.basename(opts.fileName)
+    //     },
+    //     uploadSize: size,
+    //     onError: function (error) {
+    //       throw error
+    //     },
+    //     onProgress: function (bytesUploaded, bytesTotal) {
+    //       var percentage = (bytesUploaded / bytesTotal * 100).toFixed(2)
+    //       console.log(bytesUploaded, bytesTotal, percentage + '%')
 
-//           wss.emit('google:' + token, JSON.stringify({
-//             action: 'progress',
-//             payload: {
-//               progress: percentage,
-//               bytesUploaded: bytesUploaded,
-//               bytesTotal: bytesTotal
-//             }
-//           }))
-//         },
-//         onSuccess: function () {
-//           console.log('Upload finished:', upload.url)
-//           wss.emit('google:' + token, JSON.stringify({
-//             action: 'progress',
-//             payload: {
-//               fileId: 'foo',
-//               complete: true
-//             }
-//           }))
-//         }
-//       }
+    //       wss.emit('google:' + token, JSON.stringify({
+    //         action: 'progress',
+    //         payload: {
+    //           progress: percentage,
+    //           bytesUploaded: bytesUploaded,
+    //           bytesTotal: bytesTotal
+    //         }
+    //       }))
+    //     },
+    //     onSuccess: function () {
+    //       console.log('Upload finished:', upload.url)
+    //       wss.emit('google:' + token, JSON.stringify({
+    //         action: 'progress',
+    //         payload: {
+    //           fileId: 'foo',
+    //           complete: true
+    //         }
+    //       }))
+    //     }
+    //   }
 
-//       var upload = new tus.Upload(file, options)
-//       upload.start()
-//       self.body = {
-//         token: token
-//       }
-//       self.status = 200
-//       return cb()
-//     }
+    //   var upload = new tus.Upload(file, options)
+    //   upload.start()
+    //   self.body = {
+    //     token: token
+    //   }
+    //   self.status = 200
+    //   return cb()
+    // }
 
-//     fs.readFile(opts.fileName, function (err, data) {
-//       if (err) {
-//         console.log(err)
-//         return
-//       }
+    fs.readFile(opts.fileName, function (err, data) {
+      if (err) {
+        console.log(err)
+        return
+      }
 
-//       var req = http.request({
-//         host: 'api2.transloadit.com',
-//         method: 'POST',
-//         'Content-Type': 'multipart/form-data',
-//         'Content-Length': data.length
-//       }, (res) => {
-//         console.log('STATUS:', res.statusCode)
-//         console.log('HEADERS:', JSON.stringify(res.headers, null, '\t'))
+      var req = http.request({
+        host: 'api2.transloadit.com',
+        method: 'POST',
+        'Content-Type': 'multipart/form-data',
+        'Content-Length': data.length
+      }, (res) => {
+        console.log('STATUS:', res.statusCode)
+        console.log('HEADERS:', JSON.stringify(res.headers, null, '\t'))
 
-//         res.on('data', (chunk) => {
-//           console.log('BODY:', chunk)
-//         })
+        res.on('data', (chunk) => {
+          console.log('BODY:', chunk)
+        })
 
-//         res.on('end', () => {
-//           console.log('No more data in response.')
+        res.on('end', () => {
+          console.log('No more data in response.')
 
-//           if (res.status) {
-//             self.status = res.status
-//           }
+          if (res.status) {
+            self.status = res.status
+          }
 
-//           if (res.statusCode >= 200 && res.statusCode <= 300) {
-//             // Server logging
-//             console.log('Transfer to server `' + opts.target + '` was successful.')
-//             console.log('Status code: ', res.statusCode)
+          if (res.statusCode >= 200 && res.statusCode <= 300) {
+            // Server logging
+            console.log('Transfer to server `' + opts.target + '` was successful.')
+            console.log('Status code: ', res.statusCode)
 
-//             self.status = res.statusCode
-//             return cb()
-//           }
+            self.status = res.statusCode
+            return cb()
+          }
 
-//           // Server logging
-//           console.log('Status Code was not between 200-300.  There was an error: ')
-//           console.log('response status code:', res.statusCode)
-//           console.log('response status:')
-//           console.log(res.status)
+          // Server logging
+          console.log('Status Code was not between 200-300.  There was an error: ')
+          console.log('response status code:', res.statusCode)
+          console.log('response status:')
+          console.log(res.status)
 
-//           self.status = res.statusCode
-//           return cb()
-//         })
-//       })
+          self.status = res.statusCode
+          return cb()
+        })
+      })
 
-//       req.on('error', (e) => {
-//         console.log(`problem with request: ${e.message}`)
-//       })
+      req.on('error', (e) => {
+        console.log(`problem with request: ${e.message}`)
+      })
 
-//       req.write(data)
-//       req.end()
-//     })
-//   })
+      req.write(data)
+      req.end()
+    })
+  })
 
-//   return writer
-// }
+  return writer
+}
 
 /**
  * Fetch a file from Google Drive
@@ -174,8 +174,8 @@ module.exports = function * (next) {
   })
 
   var fileId = this.request.body.fileId
-  // var target = this.request.body.target
-  // var protocol = this.request.body.protocol
+  var target = this.request.body.target
+  var protocol = this.request.body.protocol
 
   yield function getFile (cb) {
     if (!fileId) {
@@ -188,8 +188,8 @@ module.exports = function * (next) {
     google.query()
       .get('files/' + fileId)
       .request((err, res, file) => {
-        // var writer
-        // var opts
+        var writer
+        var opts
 
         if (err) {
           self.status = 500
@@ -197,68 +197,62 @@ module.exports = function * (next) {
           return cb()
         }
 
-        self.body = {
-          ok: 'yes!@'
+        if (isGoogleFile(file)) {
+          var mimeType = getFileMimeType(file.mimeType)
+          var extension = getFileExtension(file.mimeType)
+
+          if (!mimeType) {
+            self.status = 500
+            self.statusText = 'Uppy Server cannot export this type of file'
+            return cb()
+          }
+
+          opts = {
+            fileName: './output/' + file.title + extension,
+            target: target,
+            protocol: protocol
+          }
+
+          writer = getUploadStream(opts, cb, self)
+
+          google.get('files/' + fileId + '/export', {
+            qs: {
+              mimeType: mimeType
+            }
+          }, (err, res, body) => {
+            if (err) {
+              self.status = res.status
+              self.statusText = res.statusText
+              return cb()
+            }
+
+            console.log('Saving exported file with content-type: `' + res.headers['content-type'] + '` as export mimeType `' + mimeType + '` to `./output/' + file.title + extension + '`')
+          })
+          .pipe(writer)
+        } else {
+          opts = {
+            fileName: './output/' + file.title,
+            target: target,
+            protocol: protocol
+          }
+
+          writer = getUploadStream(opts, cb, self)
+
+          google.get('files/' + fileId, {
+            qs: {
+              alt: 'media'
+            }
+          }, (err, res, body) => {
+            if (err) {
+              self.status = res.status
+              self.statusText = res.statusText
+              return cb()
+            }
+
+            console.log('Saving regular file with content-type: `' + res.headers['content-type'] + '` to `./output/' + file.title + '`')
+          })
+          .pipe(writer)
         }
-
-        return cb()
-
-        // if (isGoogleFile(file)) {
-        //   var mimeType = getFileMimeType(file.mimeType)
-        //   var extension = getFileExtension(file.mimeType)
-
-        //   if (!mimeType) {
-        //     self.status = 500
-        //     self.statusText = 'Uppy Server cannot export this type of file'
-        //     return cb()
-        //   }
-
-        //   opts = {
-        //     fileName: './output/' + file.title + extension,
-        //     target: target,
-        //     protocol: protocol
-        //   }
-
-        //   writer = getUploadStream(opts, cb, self)
-
-        //   google.get('files/' + fileId + '/export', {
-        //     qs: {
-        //       mimeType: mimeType
-        //     }
-        //   }, (err, res, body) => {
-        //     if (err) {
-        //       self.status = res.status
-        //       self.statusText = res.statusText
-        //       return cb()
-        //     }
-
-        //     console.log('Saving exported file with content-type: `' + res.headers['content-type'] + '` as export mimeType `' + mimeType + '` to `./output/' + file.title + extension + '`')
-        //   })
-        //   .pipe(writer)
-        // } else {
-        //   opts = {
-        //     fileName: './output/' + file.title,
-        //     target: target,
-        //     protocol: protocol
-        //   }
-
-        //   writer = getUploadStream(opts, cb, self)
-
-        //   google.get('files/' + fileId, {
-        //     qs: {
-        //       alt: 'media'
-        //     }
-        //   }, (err, res, body) => {
-        //     if (err) {
-        //       self.status = res.status
-        //       self.statusText = res.statusText
-        //       return cb()
-        //     }
-
-        //     console.log('Saving regular file with content-type: `' + res.headers['content-type'] + '` to `./output/' + file.title + '`')
-        //   })
-        //   .pipe(writer)
-        // }
       })
   }
 }

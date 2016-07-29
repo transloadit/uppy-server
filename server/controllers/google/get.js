747b4676d666e0574901bcef86dabf2c1735c981
Harry Hedger <hedgerh@gmail.com>
2016-07-29 18:43:44 -0400

    More debug

--- server/controllers/google/get.js
+++ server/controllers/google/get.js
@@ -1,9 +1,9 @@
 var fs = require('fs')
-var http = require('http')
-// var path = require('path')
-// var tus = require('tus-js-client')
-// var wss = require('../../../WebsocketServer')
-// var generateUUID = require('../../../utils/generateUUID')
+// var http = require('http')
+var path = require('path')
+var tus = require('tus-js-client')
+var wss = require('../../../WebsocketServer')
+var generateUUID = require('../../../utils/generateUUID')
 
 var googleFileTypes = {
   document: {
@@ -49,108 +49,108 @@ function getUploadStream (opts, cb, self) {
     }
 
     // if (opts.protocol === 'tus') {
-    //   var token = generateUUID()
-    //   console.log('tus upload')
-    //   var filePath = opts.fileName
-    //   var file = fs.createReadStream(filePath)
-    //   var size = fs.statSync(filePath).size
-    //   var options = {
-    //     endpoint: opts.target,
-    //     resume: true,
-    //     metadata: {
-    //       filename: path.basename(opts.fileName)
-    //     },
-    //     uploadSize: size,
-    //     onError: function (error) {
-    //       throw error
-    //     },
-    //     onProgress: function (bytesUploaded, bytesTotal) {
-    //       var percentage = (bytesUploaded / bytesTotal * 100).toFixed(2)
-    //       console.log(bytesUploaded, bytesTotal, percentage + '%')
+    var token = generateUUID()
+    console.log('tus upload')
+    var filePath = opts.fileName
+    var file = fs.createReadStream(filePath)
+    var size = fs.statSync(filePath).size
+    var options = {
+      endpoint: opts.target,
+      resume: true,
+      metadata: {
+        filename: path.basename(opts.fileName)
+      },
+      uploadSize: size,
+      onError: function (error) {
+        throw error
+      },
+      onProgress: function (bytesUploaded, bytesTotal) {
+        var percentage = (bytesUploaded / bytesTotal * 100).toFixed(2)
+        console.log(bytesUploaded, bytesTotal, percentage + '%')
 
-    //       wss.emit('google:' + token, JSON.stringify({
-    //         action: 'progress',
-    //         payload: {
-    //           progress: percentage,
-    //           bytesUploaded: bytesUploaded,
-    //           bytesTotal: bytesTotal
-    //         }
-    //       }))
-    //     },
-    //     onSuccess: function () {
-    //       console.log('Upload finished:', upload.url)
-    //       wss.emit('google:' + token, JSON.stringify({
-    //         action: 'progress',
-    //         payload: {
-    //           fileId: 'foo',
-    //           complete: true
-    //         }
-    //       }))
-    //     }
-    //   }
+        wss.emit('google:' + token, JSON.stringify({
+          action: 'progress',
+          payload: {
+            progress: percentage,
+            bytesUploaded: bytesUploaded,
+            bytesTotal: bytesTotal
+          }
+        }))
+      },
+      onSuccess: function () {
+        console.log('Upload finished:', upload.url)
+        wss.emit('google:' + token, JSON.stringify({
+          action: 'progress',
+          payload: {
+            fileId: 'foo',
+            complete: true
+          }
+        }))
+      }
+    }
 
-    //   var upload = new tus.Upload(file, options)
-    //   upload.start()
-    //   self.body = {
-    //     token: token
-    //   }
-    //   self.status = 200
-    //   return cb()
+    var upload = new tus.Upload(file, options)
+    upload.start()
+    self.body = {
+      token: token
+    }
+    self.status = 200
+    return cb()
     // }
 
-    fs.readFile(opts.fileName, function (err, data) {
-      if (err) {
-        console.log(err)
-        return
-      }
+    // fs.readFile(opts.fileName, function (err, data) {
+    //   if (err) {
+    //     console.log(err)
+    //     return
+    //   }
 
-      var req = http.request({
-        host: 'api2.transloadit.com',
-        method: 'POST',
-        'Content-Type': 'multipart/form-data',
-        'Content-Length': data.length
-      }, (res) => {
-        console.log('STATUS:', res.statusCode)
-        console.log('HEADERS:', JSON.stringify(res.headers, null, '\t'))
+    //   var req = http.request({
+    //     host: 'api2.transloadit.com',
+    //     method: 'POST',
+    //     'Content-Type': 'multipart/form-data',
+    //     'Content-Length': data.length
+    //   }, (res) => {
+    //     console.log('STATUS:', res.statusCode)
+    //     console.log('HEADERS:', JSON.stringify(res.headers, null, '\t'))
 
-        res.on('data', (chunk) => {
-          console.log('BODY:', chunk)
-        })
+    //     res.on('data', (chunk) => {
+    //       console.log('BODY:', chunk)
+    //     })
 
-        res.on('end', () => {
-          console.log('No more data in response.')
+    //     res.on('end', () => {
+    //       console.log('No more data in response.')
 
-          if (res.status) {
-            self.status = res.status
-          }
+    //       if (res.status) {
+    //         self.status = res.status
+    //       }
 
-          if (res.statusCode >= 200 && res.statusCode <= 300) {
-            // Server logging
-            console.log('Transfer to server `' + opts.target + '` was successful.')
-            console.log('Status code: ', res.statusCode)
+    //       if (res.statusCode >= 200 && res.statusCode <= 300) {
+    //         // Server logging
+    //         console.log('Transfer to server `' + opts.target + '` was successful.')
+    //         console.log('Status code: ', res.statusCode)
 
-            self.status = res.statusCode
-            return cb()
-          }
+    //         self.status = res.statusCode
+    //         return cb()
+    //       }
 
-          // Server logging
-          console.log('Status Code was not between 200-300.  There was an error: ')
-          console.log('response status code:', res.statusCode)
-          console.log('response status:')
-          console.log(res.status)
+    //       // Server logging
+    //       console.log('Status Code was not between 200-300.  There was an error: ')
+    //       console.log('response status code:', res.statusCode)
+    //       console.log('response status:')
+    //       console.log(res.status)
 
-          self.status = res.statusCode
-          return cb()
-        })
-      })
+    //       self.status = res.statusCode
+    //       return cb()
+    //     })
+    //   })
 
-      req.on('error', (e) => {
-        console.log(`problem with request: ${e.message}`)
-      })
+    //   req.on('error', (e) => {
+    //     console.log(`problem with request: ${e.message}`)
+    //   })
 
-      req.write(data)
-      req.end()
-    })
+    //   req.write(data)
+    //   req.end()
+    // })
   })
 
   return writer

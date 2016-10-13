20392cc024882b76254349ddb7e798d32a866c28
Harry Hedger <hedgerh@gmail.com>
2016-10-13 17:09:00 -0400

    Added welcome message to index route

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,3 +1,12 @@
+var router = require('koa-router')()
+
 module.exports = function (app) {
   require('./google')(app)
+  router.get('/', function * (next) {
+    this.body = [
+      'Welcome to Uppy Server',
+      '======================',
+      ''
+    ].join('\n')
+  })
 }

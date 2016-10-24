4567a0fded243c3f55822454916dd6c522e19e70
Kevin van Zonneveld <kevin@vanzonneveld.net>
2016-10-24 18:40:27 +0200

    Fix InsecurePlatformWarning (maybe)
    http://stackoverflow.com/questions/29134512/insecureplatformwarning-a-tr
    ue-sslcontext-object-is-not-available-this-prevent

--- scripts/travis-deploy.sh
+++ scripts/travis-deploy.sh
@@ -8,7 +8,12 @@ set -o nounset
 # There is a bug with PyCParser - See https://github.com/pyca/cryptography/issues/3187
 yes w |sudo -HE \
     env PATH=${PATH} LD_LIBRARY_PATH=${LD_LIBRARY_PATH:-} PYTHONPATH=${PYTHONPATH:-} \
-    pip install pycparser==2.16
+    pip install \
+      --ignore-installed \
+      --force-reinstall \
+      --disable-pip-version-check \
+      requests==2.5.3 \
+      pycparser==2.16
 
 # because a Travis deploy script has to be a real file
 npm run deploy:travis

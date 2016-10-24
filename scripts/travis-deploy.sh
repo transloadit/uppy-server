99f263d67d18c2da9a1b08a4f583662ad95d85fe
Kevin van Zonneveld <kevin@vanzonneveld.net>
2016-10-24 18:30:42 +0200

    Don't fail on LD_LIBRARY_PATH

--- scripts/travis-deploy.sh
+++ scripts/travis-deploy.sh
@@ -7,7 +7,7 @@ set -o nounset
 # http://stackoverflow.com/questions/39829473/cryptography-assertionerror-sorry-but-this-version-only-supports-100-named-gro
 # There is a bug with PyCParser - See https://github.com/pyca/cryptography/issues/3187
 yes w |sudo -HE \
-    env PATH=${PATH} LD_LIBRARY_PATH=${LD_LIBRARY_PATH} PYTHONPATH=${PYTHONPATH} \
+    env PATH=${PATH} LD_LIBRARY_PATH=${LD_LIBRARY_PATH:-} PYTHONPATH=${PYTHONPATH:-} \
     pip install pycparser==2.16
 
 # because a Travis deploy script has to be a real file

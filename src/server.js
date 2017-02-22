c9a2fad598fd519a68f8d26c69b4ebb17235512b
Ifedapo Olarewaju <ifedapoolarewaju@gmail.com>
2017-02-23 00:07:34 +0100

    debug: localhost with cors breaks

--- src/server.js
+++ src/server.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
     protocol = req.headers.origin.startsWith('https') ? 'https' : 'http'
   }
 
-  if (req.headers.origin.startsWith('http://localhost:')) {
+  if (req.headers.origin && req.headers.origin.startsWith('http://localhost:')) {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3452')
   } else {
     res.setHeader(

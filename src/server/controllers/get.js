fc21966f787bbd46becd1ac0fb1c1d0ac591a7ae
Ifedapo Olarewaju <ifedapoolarewaju@gmail.com>
2017-05-18 10:22:07 +0100

    refactor: remove unnecessary variable declarations

--- src/server/controllers/get.js
+++ src/server/controllers/get.js
@@ -6,15 +6,13 @@ function get (req, res) {
   const providerName = req.params.providerName
   const id = req.params.id
   const body = req.body
-  const endpoint = body.endpoint
-  const protocol = body.protocol
   const token = req.session[providerName]
     ? req.session[providerName].token
     : body.token
   const provider = utils.getProvider({ providerName, config })
   const uploader = new Uploader({
-    endpoint,
-    protocol,
+    endpoint: body.endpoint,
+    protocol: body.protocol,
     size: body.size,
     path: `${process.env.UPPYSERVER_DATADIR}/${encodeURIComponent(id)}`
   })

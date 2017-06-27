8712499acac7443f6dd3a57aeffd48e0b64fe6e8
Ifedapo Olarewaju <ifedapoolarewaju@gmail.com>
2017-06-27 18:59:02 +0100

    fix: download first file for instagram carousels

--- src/server/provider/instagram.js
+++ src/server/provider/instagram.js
@@ -16,14 +16,30 @@ class Instagram {
       .request(done)
   }
 
+  _getMediaUrl (body) {
+    let mediaObj
+    let type
+
+    if (body.data.type === 'carousel') {
+      // carousels are not yet supported,
+      // so we download just the first file.
+      mediaObj = body.data.carousel_media[0]
+      type = mediaObj.type
+    } else {
+      mediaObj = body.data
+      type = body.data.type
+    }
+
+    return mediaObj[`${type}s`].standard_resolution.url
+  }
+
   download ({ id, token }, onData, onResponse) {
     return this.client
       .get(`media/${id}`)
       .auth(token)
       .request((err, resp, body) => {
         if (err) return console.log('there was an error:', err)
-
-        request(body.data[`${body.data.type}s`].standard_resolution.url)
+        request(this._getMediaUrl(body))
           .on('response', onResponse)
           .on('error', (err) => {
             console.log('there was an error:', err)

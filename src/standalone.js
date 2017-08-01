73698fd0cfeb728ea9973cbfcec5f96ddd231397
Ifedapo Olarewaju <ifedapoolarewaju@gmail.com>
2017-08-01 11:19:15 +0100

    remove cookie max age

--- src/standalone.js
+++ src/standalone.js
@@ -38,10 +38,7 @@ app.disable('x-powered-by')
 const sessionOptions = {
   secret: process.env.UPPYSERVER_SECRET,
   resave: true,
-  saveUninitialized: false,
-  cookie: {
-    maxAge: 1000 * 60 * 60 * 24 // 24 hours
-  }
+  saveUninitialized: true
 }
 
 if (process.env.UPPYSERVER_REDIS_URL) {

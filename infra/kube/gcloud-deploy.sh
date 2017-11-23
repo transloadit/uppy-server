9cf6635160e73405f55943f9a5bf91e3c99fd705
kiloreux <kiloreux@gmail.com>
2017-11-23 22:11:51 +0100

    debug secret travis key

--- infra/kube/gcloud-deploy.sh
+++ infra/kube/gcloud-deploy.sh
@@ -5,7 +5,9 @@ __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 __kube="${__dir}"
 
 
-echo -n `echo $GCLOUD_KEY | base64 --decode -i` > ${HOME}/gcloud-service-key.json
+echo $GCLOUD_KEY | base64 --decode -i > ${HOME}/gcloud-service-key.json
+# This is a useless key, just for debugging 
+cat ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 
 echo $UPPY_ENV | base64 --decode -i > "${__kube}/uppy-server/uppy-env.yaml"

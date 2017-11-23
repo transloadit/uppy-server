652598f2e58876ecc791e741e985b1efc1f929f1
kiloreux <kiloreux@gmail.com>
2017-11-23 23:19:56 +0100

    All hail travis

--- infra/kube/gcloud-deploy.sh
+++ infra/kube/gcloud-deploy.sh
@@ -1,14 +1,24 @@
 #!/usr/bin/env bash
+set -o pipefail
+set -o errexit
+set -o nounset
+# set -o xtrace
+
+
 
 # Set magic variables for current FILE & DIR
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 __kube="${__dir}"
 
+# Store the new image in docker hub
+docker build --quiet -t transloadit/uppy-server:latest -t transloadit/uppy-server:$TRAVIS_COMMIT .;
+docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
+docker push transloadit/uppy-server:$TRAVIS_COMMIT;
+docker push transloadit/uppy-server:latest;
 
-echo $GCLOUD_KEY | base64 --decode -i > gcloud-service-key.json
-# This is a useless key, just for debugging 
-cat gcloud-service-key.json
-gcloud auth activate-service-account --key-file gcloud-service-key.json
+
+echo $GCLOUD_KEY | base64 --decode -i > ${HOME}/gcloud-service-key.json
+gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 
 echo $UPPY_ENV | base64 --decode -i > "${__kube}/uppy-server/uppy-env.yaml"
 

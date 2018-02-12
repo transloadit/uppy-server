6cc67b3c4facfeb3533564ccda62c85ec72f31b1
Ifedapo Olarewaju <ifedapoolarewaju@gmail.com>
2018-02-12 14:31:18 +0100

    log uppy version in standalone server

--- src/standalone/start-server.js
+++ src/standalone/start-server.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 const uppy = require('../uppy')
+// @ts-ignore
+const { version } = require('../../package.json')
 const { app, uppyOptions } = require('./index')
 const PORT = process.env.UPPYSERVER_PORT || 3020
 
 uppy.socket(app.listen(PORT), uppyOptions)
 
-console.log('Welcome to Uppy Server!')
+console.log(`Welcome to Uppy Server! v${version}`)
 console.log(`Listening on http://0.0.0.0:${PORT}`)

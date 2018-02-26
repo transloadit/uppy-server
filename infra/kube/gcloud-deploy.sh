7cc54f9f31e495c17a29342a79d18a7c57a13b6c
kiloreux <kiloreux@gmail.com>
2018-02-26 19:26:25 +0100

    Bring things back

--- infra/kube/gcloud-deploy.sh
+++ infra/kube/gcloud-deploy.sh
@@ -9,39 +9,36 @@ __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 __kube="${__dir}"
 
 # Store the new image in docker hub
-# docker build --quiet -t transloadit/uppy-server:latest -t transloadit/uppy-server:$TRAVIS_COMMIT .;
-# docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
-# docker push transloadit/uppy-server:$TRAVIS_COMMIT;
-# docker push transloadit/uppy-server:latest;
+docker build --quiet -t transloadit/uppy-server:latest -t transloadit/uppy-server:$TRAVIS_COMMIT .;
+docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
+docker push transloadit/uppy-server:$TRAVIS_COMMIT;
+docker push transloadit/uppy-server:latest;
 
-# echo $CA_CRT | base64 --decode -i > ${HOME}/ca.crt
+echo $CA_CRT | base64 --decode -i > ${HOME}/ca.crt
 
-# gcloud config set container/use_client_certificate True
-# export CLOUDSDK_CONTAINER_USE_CLIENT_CERTIFICATE=True
+gcloud config set container/use_client_certificate True
+export CLOUDSDK_CONTAINER_USE_CLIENT_CERTIFICATE=True
 
-# kubectl config set-cluster transloadit-cluster --embed-certs=true --server=${CLUSTER_ENDPOINT} --certificate-authority=${HOME}/ca.crt
-# kubectl config set-credentials travis --token=$SA_TOKEN
-# kubectl config set-context travis --cluster=$CLUSTER_NAME --user=travis --namespace=uppy
-# kubectl config use-context travis
+kubectl config set-cluster transloadit-cluster --embed-certs=true --server=${CLUSTER_ENDPOINT} --certificate-authority=${HOME}/ca.crt
+kubectl config set-credentials travis --token=$SA_TOKEN
+kubectl config set-context travis --cluster=$CLUSTER_NAME --user=travis --namespace=uppy
+kubectl config use-context travis
 
-rm -f "${__kube}/uppy-server/uppy-env.yaml"
-echo $UPPY_ENV
-echo $UPPY_ENV | base64 --decode 
-echo $UPPY_ENV | base64 --decode   > "${__kube}/uppy-server/uppy-env.yaml"
+echo $UPPY_ENV | base64 --decode > "${__kube}/uppy-server/uppy-env.yaml"
 
-# kubectl config current-context
+kubectl config current-context
 
-# kubectl apply -f "${__kube}/uppy-server/uppy-env.yaml"
-# sed -i "s#NFS_SERVER_IP#${NFS_SERVER_IP}#" "${__kube}/uppy-server/nfs.yaml"
-# kubectl apply -f "${__kube}/uppy-server/nfs.yaml"
-# sleep 10s # This cost me some precious debugging time.
-# kubectl apply -f "${__kube}/uppy-server/uppy-server-kube.yaml"
-# kubectl set image deployment/uppy-server --namespace=uppy uppy-server=docker.io/transloadit/uppy-server:$TRAVIS_COMMIT
-# sleep 10s
+kubectl apply -f "${__kube}/uppy-server/uppy-env.yaml"
+sed -i "s#NFS_SERVER_IP#${NFS_SERVER_IP}#" "${__kube}/uppy-server/nfs.yaml"
+kubectl apply -f "${__kube}/uppy-server/nfs.yaml"
+sleep 10s # This cost me some precious debugging time.
+kubectl apply -f "${__kube}/uppy-server/uppy-server-kube.yaml"
+kubectl set image deployment/uppy-server --namespace=uppy uppy-server=docker.io/transloadit/uppy-server:$TRAVIS_COMMIT
+sleep 10s
 
-# kubectl get pods --namespace=uppy
-# kubectl get service --namespace=uppy
-# kubectl get deployment --namespace=uppy
+kubectl get pods --namespace=uppy
+kubectl get service --namespace=uppy
+kubectl get deployment --namespace=uppy
 
 function cleanup {
     printf "Cleaning up...\n"
